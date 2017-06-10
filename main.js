92aeabdaf897ab023662ea76ab6120c6328b23f0
mcgrawga <garthmcgraw@statgolf.com>
2017-06-10 12:22:01 -0500

    Modified package.json

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ app.get('/basketball/:statline', function (req, res) {
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
-   console.log("App listening at http://%s:%s", host, port)
+   console.log("Statchomper listening at http://%s:%s", host, port)
 });
  
 module.exports = app;
