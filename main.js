25f80dac19bbdccec7391aacf794a90e6a12eb49
mcgrawga <garthmcgraw@statgolf.com>
2018-08-20 11:53:54 -0500

    Allowed CORS

--- main.js
+++ main.js
@@ -7,8 +7,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 var format = require('string-format');
 format.extend(String.prototype);
 var MongoClient = require('mongodb').MongoClient;
-
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
  
 // REMOVE UNWANTED CHARACTERS
 function cleanStatLine(statLine){
